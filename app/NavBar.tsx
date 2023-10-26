f97f929f53bbe05a1eb15e81555a64c8fc3c575c
bra-nat <126584626+bra-nat@users.noreply.github.com>
2023-10-26 04:15:48 +0000

    innitial and built navbar commits

---
+++ app/NavBar.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import classnames from 'classnames'
+
+const NavBar = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
+  return (
+    <nav className="flex space-x-6 px-4 border-b mb-5 h-14 items-center">
+      <Link href="/">{<AiFillBug />}</Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li
+            key={link.href}
+            className={classnames({
+              'text-zinc-900': link.href === currentPath,
+              'text-zinc-500': link.href !== currentPath,
+              'hover:text-zinc-800 transition-colors': true,
+            })}>
+            <Link href={link.href}>{link.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
